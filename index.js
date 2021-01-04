b0ebb3e6c71f45f01d542b80a62e01a38280df07
Scott Davies <scottdavies1983@gmail.com>
2021-01-04 18:57:50 +0000

    package exploration

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 import wait from "waait";
 import faker from "faker";
+// import {name} from 'faker';
+import { formatDistance, format } from "date-fns";
 
-console.log(faker);
+console.log(`Hello ${faker.name.firstName()}`);
+
+const fakeNames = Array.from(
+  { length: 10 },
+  () => `${faker.name.firstName()} ${faker.name.lastName()}`
+);
+
+console.log(fakeNames);
 
 async function go() {
   console.log("Going!");
@@ -10,3 +19,18 @@ async function go() {
 }
 
 go();
+
+const diff = formatDistance(
+  new Date(1986, 3, 4, 11, 32, 0),
+  new Date(1986, 3, 4, 10, 32, 0),
+  { addSuffix: true }
+); //= > 'in about 1 hour'
+
+console.log(diff);
+
+const date = new Date();
+
+// January the 12th 2020
+const formatted = format(date, "LLLL 'the' do y");
+
+console.log(formatted);
